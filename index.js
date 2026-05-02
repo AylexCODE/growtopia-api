d2e3fa0b7d4514a189340a30fc979370f7bcf549
Aylex <130986014+AylexCODE@users.noreply.github.com>
2026-05-02 03:45:01 +0000

    Get initial data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 const rateLimit = require('express-rate-limit');
 const axios = require('axios');
 
+const { load } = require('cheerio');
+
 const maxLimit = rateLimit({
     windowMs: 60 * 1000,
     max: 25,
@@ -30,10 +32,23 @@ app.get('/item', async (req, res) => {
         
         const response = await axios({
             method: "GET",
-            url: `https://growtopia.fandom.com/api.php?action=parse&page=${encodeURIComponent(resultItems[0].title)}`
+            url: `https://growtopia.fandom.com/api.php?action=parse&page=${encodeURIComponent(resultItems[0].title)}&&format=json`
         });
-        console.log(response);
-        res.sendStatus(200);
+        const html = load(response.data.parse.text["*"]);
+
+        const description = html(".card-text").first().text().trim();
+        const properties = html("#mw-content-text > div > div.gtw-card.item-card > div:nth-child(4)").text().trim().split(/[\.+\!]/).filter((d) => d !== "");
+        const sprite = html("div.card-header .growsprite > img").attr("src");
+        const color = html(".seedColor > div").text().trim()?.split(" ");
+        const rarity = html(".card-header b > small").text().match(/(\d+)/);
+        const recipe = html(".recipebox table.content").last().text().trim().split(/[\r\n\x0B\x0C\u0085\u2028\u2029]+/).map((el) => el.trim());
+        const splice = html(".bg-splice").text();
+        const info = html("#mw-content-text > div > p:nth-child(3)").text().trim();
+        const type = html("table.card-field tr:nth-child(1) > td").text().split(" ").pop();
+
+        res.send({
+            description, properties, sprite, color, rarity, recipe, splice, info, type
+        });
     }catch(e){
         console.log(e);
         res.sendStatus(500);
@@ -45,7 +60,6 @@ app.get('/search', async (req, res) => {
         const itemName = req.query.q;
 
         const response = await searchItem(itemName);
-        console.log(response);
 
         res.send(response);
     }catch(e){
